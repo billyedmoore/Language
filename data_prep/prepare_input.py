59501b05ca35375e1de06407ea4854e76b550296
Billy Moore <billyedmoore@yahoo.com>
2025-06-21 13:47:37 +0100

    Formatting and fixes.

--- data_prep/prepare_input.py
+++ data_prep/prepare_input.py
@@ -1,42 +1,44 @@
 import os
 import re
 from collections import Counter
-from model_utils import get_frequencies
+from simple_model.model_utils import get_frequencies
+from pathlib import Path
+
+CHUNK_SIZE = 1024
 
 
-def prepare_input(input_filename: str, output_filename: str, minimum_freq: int = 100):
-    """ 
+def prepare_input(input_filename: Path, output_filename: Path, minimum_freq: int = 100):
+    """
     Remove infrequent characters from file {input_filename} and save result to {output_filename}.
     """
-    if not os.path.exists(input_filename):
+    if not input_filename.exists():
         raise FileNotFoundError(f"Input file {input_filename} does not exist.")
 
     char_freq = get_frequencies(input_filename)
 
-    chars_to_remove = [char for (char,freq) in char_freq.items() if freq < minimum_freq]
-    
+    chars_to_remove = [
+        char for (char, freq) in char_freq.items() if freq < minimum_freq
+    ]
+
     with open(output_filename, "w") as f:
         f.write("")
-    
+
     with open(input_filename, "r") as input_f:
         while 1:
             chunk = input_f.read(CHUNK_SIZE)
-            chunk = re.sub("“|”",'"',chunk)
-            chunk = re.sub("’|‘","'",chunk)
-            chunk = re.sub("\n+","\n",chunk)
-            chunk = re.sub(" +"," ",chunk)
-            
-            chunk = chunk.encode("ascii","ignore").decode("ascii")
+            chunk = re.sub("“|”", '"', chunk)
+            chunk = re.sub("’|‘", "'", chunk)
+            chunk = re.sub("\n+", "\n", chunk)
+            chunk = re.sub(" +", " ", chunk)
+
+            chunk = chunk.encode("ascii", "ignore").decode("ascii")
             # If nothing left to read
             if not chunk:
                 break
 
             with open(output_filename, "a") as output_f:
-                output_f.write("".join([c for c in chunk.lower() if c not in chars_to_remove]))
+                output_f.write(
+                    "".join([c for c in chunk.lower() if c not in chars_to_remove])
+                )
 
     print(_get_frequencies(output_filename))
-
-
-
-
-
--- simple_model/__main__.py
+++ simple_model/__main__.py
@@ -1,37 +1,34 @@
 from .dataset import SimpleModelDataset
 from .model import SimpleModel, train
-from .model_utils import predict_next_char 
+from .model_utils import predict_next_char
 from data_prep import prepare_input
+from pathlib import Path
 import datetime
 import os
 import torch
 
 
-
 def main(input_length):
     simple_filename = "shakespeare"
-    input_file = f"../data/{simple_filename}.txt"
-    prepared_input = f"../data/prepared_{simple_filename}.txt"
-    print(datetime.datetime.now())
+    file_path = Path(__file__).resolve().parent.parent
+    data_folder = file_path.joinpath("data")
+    input_file = data_folder.joinpath(f"{simple_filename}.txt")
+    prepared_input = data_folder.joinpath(f"prepared_{simple_filename}.txt")
 
-    if not os.path.exists(prepared_input):
-        prepare_input(input_file,prepared_input)
+    if not prepared_input.exists():
+        prepare_input(input_file, prepared_input)
 
-
-    train_data = SimpleModelDataset(prepared_input, 10000, input_length, random_seed=913
+    train_data = SimpleModelDataset(
+        prepared_input, 10000, input_length, random_seed=913
     )
-    eval_data = SimpleModelDataset(prepared_input, 1000, input_length, random_seed=340
-    )
+    eval_data = SimpleModelDataset(prepared_input, 1000, input_length, random_seed=340)
 
-
-    model = SimpleModel(input_length,len(train_data.i_to_char))
-    print(datetime.datetime.now())
+    model = SimpleModel(input_length, len(train_data.i_to_char))
     train(model, train_data, eval_data, num_epochs=100)
-    print(datetime.datetime.now())
 
     start = "to be or "
     while len(start) < 20:
-        start += predict_next_char(model,eval_data,start)
+        start += predict_next_char(model, eval_data, start)
         print(repr(start))
 
 
