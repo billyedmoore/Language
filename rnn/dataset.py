fe7ec88aa6c23c46c8af57c50cbc87a0482bb2e0
Billy Moore <billyedmoore@yahoo.com>
2025-06-22 19:42:32 +0100

    Hello RNN.

---
+++ rnn/dataset.py
@@ -0,0 +1,96 @@
+import torch
+import random
+import os
+from pathlib import Path
+
+
+class RNNDataset(torch.utils.data.Dataset):
+    i_to_char: list[str]
+    char_to_i: dict[str, int]
+
+    def __init__(
+        self,
+        file_name: Path,
+        number_of_samples: int,
+        input_length: int,
+        random_seed: int | None = None,
+    ):
+        self.file_name = file_name
+        self._create_character_dictionary()
+        self.random = random.Random(random_seed)
+        self.number_of_samples = number_of_samples
+        self.file_length = os.path.getsize(file_name)
+        self.input_length = input_length
+
+        self.start_indexes = [
+            self.random.randint(3, self.file_length) for _ in range(number_of_samples)
+        ]
+
+    def _create_character_dictionary(self):
+        """
+        Create `i_to_char` and `char_to_i` using all chars included in the corpus.
+        """
+        unique_chars = set()
+        with open(self.file_name, "r") as f:
+            while 1:
+                chunk = f.read(1024)
+
+                # If nothing left to read
+                if not chunk:
+                    break
+
+                unique_chars.update(chunk)
+
+        char_dict = {c: i for (i, c) in enumerate(unique_chars)}
+
+        self.char_to_i = char_dict
+        char_list: list[str] = ["TO_BE_POPULATED"] * (max(char_dict.values()) + 1)
+
+        for c, i in char_dict.items():
+            char_list[i] = c
+
+        assert "TO_BE_POPULATED" not in char_list
+
+        self.i_to_char = char_list
+
+    def encode_input(self, input: str) -> torch.Tensor:
+        input_indexes = [self.char_to_i[c] for c in input]
+        index_tensor = torch.tensor(input_indexes, dtype=torch.int64)
+        input_tensor = torch.zeros(self.input_length, len(self.i_to_char))
+        input_tensor = input_tensor.scatter(1, index_tensor.unsqueeze(1), 1)
+        return input_tensor
+
+    def _encode_target(self, target: str) -> torch.Tensor:
+        raw_label_tensor = torch.tensor(
+            [self.char_to_i[c] for c in target], dtype=torch.int64
+        )
+        return raw_label_tensor
+
+    def __getitem__(self, idx):
+        """
+        Get a single (x,y) pair by id.
+        """
+        char_index = self.start_indexes[idx]
+
+        assert char_index >= 0 and char_index < self.file_length
+
+        with open(self.file_name, "r") as f:
+            f.seek(char_index)
+            seq = f.read(self.input_length + 1)
+
+        input_tensor = self.encode_input(seq[:-1])
+        label_tensor = self._encode_target(seq[1:])
+
+        return input_tensor, label_tensor
+
+    def __len__(self):
+        return self.number_of_samples
+
+    def output_to_char(self, output: torch.Tensor) -> str:
+        return self.i_to_char[int(output.cpu().tolist()[0])]
+
+    def input_to_str(self, input: torch.Tensor) -> str:
+        string = ""
+        for i in input.cpu().tolist():
+            string += self.i_to_char[int(i)]
+        return string
---
+++ rnn/model.py
@@ -0,0 +1,97 @@
+import torch
+from .dataset import RNNDataset
+import random
+
+
+class RNNModel(torch.nn.Module):
+    def __init__(self, numb_categories: int, hidden_size: int):
+        super(RNNModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.rnn = torch.nn.RNN(numb_categories, hidden_size, batch_first=True)
+        self.linear = torch.nn.Linear(hidden_size, numb_categories)
+
+    def forward(self, x, hidden_state):
+        x, hidden_state = self.rnn(x, hidden_state)
+        x = self.linear(x.reshape(-1, self.hidden_size))
+        return x, hidden_state
+
+    def create_hidden(self, batch_size: int):
+        return torch.zeros(1, batch_size, self.hidden_size)
+
+
+def train(
+    model: RNNModel,
+    dataset: RNNDataset,
+    num_epochs: int = 100,
+    learning_rate: float = 0.01,
+):
+    train_dataset, eval_dataset = torch.utils.data.random_split(dataset, [0.8, 0.2])
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=16,
+        shuffle=True,
+    )
+    eval_loader = torch.utils.data.DataLoader(
+        eval_dataset,
+        batch_size=16,
+        shuffle=True,
+    )
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(num_epochs):
+        train_loss = 0
+        for _, (inputs, targets) in enumerate(train_loader):
+            hidden = model.create_hidden(inputs.size(0))
+            optimizer.zero_grad()
+            outputs, hidden = model(inputs, hidden)
+            loss = criterion(outputs, targets.view(-1))
+            loss.backward()
+            optimizer.step()
+            train_loss += loss.item() * inputs.size(0)
+
+        model.eval()
+        eval_loss = 0.0
+        with torch.no_grad():
+            for _, (inputs, targets) in enumerate(eval_loader):
+                hidden = model.create_hidden(inputs.size(0))
+                outputs, hidden = model(inputs, hidden)
+                loss = criterion(outputs, targets.view(-1))
+                eval_loss += loss.item() * inputs.size(0)
+        model.train()
+        print(
+            f"Epoch [{epoch + 1}/{num_epochs}], "
+            f"Train Loss: {train_loss / len(train_loader):.4f}, "
+            f"Validation Loss: {eval_loss / len(eval_loader):.4f}"
+        )
+
+
+def generate_text(
+    model: RNNModel, input_str: str, length_to_add: int, dataset: RNNDataset
+):
+    model.eval()
+    hidden = model.create_hidden(1)
+
+    generated_text = input_str
+
+    for c in input_str:
+        input = torch.unsqueeze(dataset.encode_input(c), dim=0)
+        _, hidden = model(input, hidden)
+
+    previous_char = input_str[-1]
+
+    for _ in range(length_to_add):
+        input = torch.unsqueeze(dataset.encode_input(previous_char), dim=0)
+        output, hidden = model(input, hidden)
+
+        probabilites = torch.softmax(output, dim=1).tolist()[0]
+        selected = random.choices(
+            [i for i in range(len(probabilites))], weights=probabilites, k=1
+        )
+        previous_char = dataset.i_to_char[selected[0]]
+        generated_text += previous_char
+
+    model.train()
+    return generated_text
