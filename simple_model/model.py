Fixed bug where softmax is run twice in simple_model

simple_model/model.py
@@ -19,7 +19,7 @@ class SimpleModel(torch.nn.Module):
         x = self.relu1(x)
         x = self.linear2(x)
         x = self.relu2(x)
-        return self.softmax(x)
+        return x
 
 
 def _char_frequency_to_class_weights(

simple_model/model_utils.py
@@ -1,6 +1,7 @@
 import random
 from collections import Counter
 from pathlib import Path
+import torch
 
 
 def get_frequencies(input_filename: Path) -> dict[str, int]:
@@ -18,8 +19,8 @@ def get_frequencies(input_filename: Path) -> dict[str, int]:
 
 
 def predict_next_char(model, dataset, input: str):
-    input_tensor = dataset.encode_input(input).unsqueeze(0)
-    probabilites = model(input_tensor).tolist()[0]
+    input_tensor: torch.Tensor = dataset.encode_input(input).unsqueeze(0)
+    probabilites = torch.softmax(model(input_tensor), dim=1).tolist()[0]
 
     print(probabilites)
 